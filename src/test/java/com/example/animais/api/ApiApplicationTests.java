ac76289417434c608604b39c5b01bd5e5a16d63f
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2021-04-22 18:13:32 -0300

    base para aplicação web classe animais

---
+++ src/test/java/com/example/animais/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animais.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
